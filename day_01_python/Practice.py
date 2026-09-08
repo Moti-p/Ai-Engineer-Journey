df62f57110c2270fa287bb88f30a53845d637192
Moti <aklopan124@gmail.com>
2026-09-08 21:04:51 +1000

    Remove accidental Day 1 copy

--- day_01_python/Practice.py
+++
@@ -1,5 +0,0 @@
-# python
-name = "Moti"
-print("my name is", name)
-print("I am learning AI Engineering")
-print("I will be an AI Engineer soon")
--- day_01_python/practice.py
+++
@@ -1,5 +0,0 @@
-# python
-name = "Moti"
-print("my name is ", name)
-print("I am learning Ai Engineering")
-print("I will be Ai engineer soon")

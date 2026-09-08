6f0d55aebfe3e588a9ec564d35076b27ccc8ca94
Moti <aklopan124@gmail.com>
2026-09-08 22:11:12 +1000

    Add calculator menu and error handling

--- Calculator.py
+++ Calculator.py
@@ -1,12 +1,25 @@
 # python program to add two numbers
-number1 = float(input("Enter your first number: "))
-number2 = float(input("Enter your second number: "))
-addition = number1 + number2
-subtraction = number1 - number2
-multiplication = number1 * number2
-division = number1 / number2
-print("Addition:", addition)
-print("Subtraction:", subtraction)
-print("Multiplication:", multiplication)
-print("Division:", division)
-print("Thank you for using Moti's Calculator")
+print("---Simple Calculator---")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+choice = input("choose an operation (1-4): ")
+number1 = float(input("Enter first number:"))
+number2 = float(input("Enter Second number:"))
+print("you chose: ", choice)
+if choice == "1":
+    result = number1 + number2
+    print("You selected Addition, Result is:", result)
+elif choice == "2":
+    result = number1 - number2
+    print("You Selected Subtraction, Result is:", result)
+elif choice == "3":
+    result = number1 * number2
+    print("You selected Multiplication, Result is:", result)
+elif choice == "4":
+    if number2 == 0:
+        print("Error. Division by zero is not allowed.")
+else:
+    result = number1 / number2
+    print("You selected Division, Result is:", result)
